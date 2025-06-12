3c66ac5461ebee3c0890f65472986a633c1f4c8d
leeseungaa <sa20429@gmail.com>
2025-06-12 20:20:05 +0900

    CustomUserDetailsService 및 AuthenticationManager 설정 추가

--- src/main/java/com/hoshimusubi/seunga/security/SecurityConfig.java
+++ src/main/java/com/hoshimusubi/seunga/security/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.hoshimusubi.seunga.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,6 +12,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	private CustomUserDetailsService customUserDetailsService;
+
+	public SecurityConfig(CustomUserDetailsService customUserDetailsService) {
+        this.customUserDetailsService = customUserDetailsService;
+    }
 	
 	@Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -27,10 +35,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .logout()
                 .permitAll();
     }
-
+	
+	 @Override
+	    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	        auth.userDetailsService(customUserDetailsService)
+	            .passwordEncoder(passwordEncoder());
+	    }
+	
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+    
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 	
 
