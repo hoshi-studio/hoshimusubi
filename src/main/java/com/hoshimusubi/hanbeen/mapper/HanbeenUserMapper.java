c908e6029938af595d959d69e2a3072bb8c5cad9
HBKIM <hbedwin99@naver.com>
2025-06-14 13:26:44 +0900

    Update HanbeenUserMapper.java
    문법 호환되게 수정

--- src/main/java/com/hoshimusubi/hanbeen/mapper/HanbeenUserMapper.java
+++ src/main/java/com/hoshimusubi/hanbeen/mapper/HanbeenUserMapper.java
@@ -2,22 +2,19 @@ package com.hoshimusubi.hanbeen.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
-
-import com.hoshimusubi.hanbeen.dto.UserProfileDTO;
+import com.hoshimusubi.hanbeen.dto.UserProfileDTO; // ← ✅ 이거 꼭 필요!
 
 @Mapper
 public interface HanbeenUserMapper {
 
-    @Select("""
-        SELECT 
-            u.nickname,
-            u.gender,
-            u.user_id AS email,
-            z.koname_ja AS zodiacNameJa,
-            u.profile_pic AS profilePic
-        FROM Users u
-        JOIN Zodiac z ON u.zodiac_id = z.id
-        WHERE u.id = #{userId}
-    """)
+    @Select("SELECT " +
+            "u.nickname, " +
+            "u.gender, " +
+            "u.user_id AS email, " +
+            "z.koname_ja AS zodiacNameJa, " +
+            "u.profile_pic AS profilePic " +
+            "FROM Users u " +
+            "JOIN Zodiac z ON u.zodiac_id = z.id " +
+            "WHERE u.id = #{userId}")
     UserProfileDTO getUserProfileById(Long userId);
 }
