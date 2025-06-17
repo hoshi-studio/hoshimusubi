9fe501645273938150e0d87c1240d94974bad0fd
leeseungaa <sa20429@gmail.com>
2025-06-17 16:20:26 +0900

    소셜로그인 기능 추가

--- src/main/java/com/hoshimusubi/seunga/security/SecurityConfig.java
+++ src/main/java/com/hoshimusubi/seunga/security/SecurityConfig.java
@@ -1,6 +1,9 @@
 package com.hoshimusubi.seunga.security;
 
+import java.io.InputStream;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -9,31 +12,56 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hoshimusubi.seunga.mapper.UserMapper;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	private CustomUserDetailsService customUserDetailsService;
-
+	private CustomOAuth2UserService customOAuth2UserService;
+	
 	@Autowired
-	public SecurityConfig(CustomUserDetailsService customUserDetailsService) {
+	private UserMapper userMapper;
+
+
+    @Autowired
+    public SecurityConfig(CustomUserDetailsService customUserDetailsService, 
+                          CustomOAuth2UserService customOAuth2UserService) {
         this.customUserDetailsService = customUserDetailsService;
+        this.customOAuth2UserService = customOAuth2UserService;
     }
+	
 	
 	@Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
+		http
         	.csrf().disable()
             .authorizeRequests()
-            	.antMatchers("/", "/login", "/resources/**", "/css/**", "/js/**").permitAll()
+            	.antMatchers("/", "/login",  "/signupExtra","/dosignupExtra", "/oauth2Redirect", "/resources/**", "/css/**", "/js/**").permitAll()
             	.anyRequest().hasRole("USER")
             .and()
+            .oauth2Login()
+            	.loginPage("/login")
+            	.userInfoEndpoint()
+                	.userService(customOAuth2UserService)
+            .and()
+            .defaultSuccessUrl("/oauth2Redirect")
+            .and() 
             .formLogin()
                 .loginPage("/login")
                 .loginProcessingUrl("/dologin")
-                .defaultSuccessUrl("/")
+                .defaultSuccessUrl("/", true)
                 .failureUrl("/login?error=true") // 실패 시 이동
                 .permitAll()
                 .and()
@@ -60,5 +88,33 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return super.authenticationManagerBean();
     }
     
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+    	
+    	ObjectMapper mapper = new ObjectMapper();
+        InputStream is = getClass().getClassLoader().getResourceAsStream("oauth2.json");
+        JsonNode root = mapper.readTree(is);
+        JsonNode googleNode = root.path("google");
+
+        String clientId = googleNode.path("clientId").asText();
+        String clientSecret = googleNode.path("clientSecret").asText();
+    	
+    	
+        ClientRegistration google = ClientRegistration.withRegistrationId("google")
+            .clientId(clientId)
+            .clientSecret(clientSecret)
+            .scope("profile", "email")
+            .authorizationUri("https://accounts.google.com/o/oauth2/v2/auth")
+            .tokenUri("https://oauth2.googleapis.com/token")
+            .userInfoUri("https://openidconnect.googleapis.com/v1/userinfo")
+            .userNameAttributeName("sub")
+            .clientName("Google")
+            .redirectUriTemplate("{baseUrl}/login/oauth2/code/{registrationId}")
+            .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+            .build();
+
+        return new InMemoryClientRegistrationRepository(google);
+    }
+   
 
 }
