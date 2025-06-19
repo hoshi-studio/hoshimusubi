9b81f147ae38587c831856f6c86fa807de3982dc
leeseungaa <sa20429@gmail.com>
2025-06-19 15:12:16 +0900

    로그인시 빈 입력값인지 확인, 스페이스 입력 방지

--- src/main/webapp/resources/js/modal.js
+++ src/main/webapp/resources/js/modal.js
@@ -1,14 +1,72 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const form = document.getElementById("loginForm");
+  const idInput = document.getElementById("id");
+  const pwInput = document.getElementById("password");
+  
+    idInput.addEventListener("keydown", function(e) {
+        if (e.key === " ") {
+          e.preventDefault();
+        }
+      });
 
-  function showModal(type) {
+    idInput.addEventListener("input", function() {
+        this.value = this.value.replace(/\s/g, "");
+      });
+  
+    pwInput.addEventListener("keydown", function(e) {
+      if (e.key === " ") {
+        e.preventDefault();
+      }
+    });
+
+    pwInput.addEventListener("input", function() {
+      this.value = this.value.replace(/\s/g, "");
+    });
+
+  form.addEventListener("submit", function(e) {
+    // ID 빈값 검사
+    if (idInput.value.trim() === "") {
+      showModal("errorid");
+      idInput.focus();
+      e.preventDefault(); // 서버 전송 막음
+      return;
+    }
+
+    // PW 빈값 검사
+    if (pwInput.value.trim() === "") {
+      showModal("errorpw");
+      pwInput.focus();
+      e.preventDefault();
+      return;
+    }
+
+    // 둘 다 OK면 서버로 제출
+  });
+});
+  
+function showModal(type) {
     document.getElementById('modalOverlay').style.display = 'block';
+    document.getElementById('successModal').style.display = 'none';
+    document.getElementById('errorModal').style.display = 'none';
+    document.getElementById('erroridModal').style.display = 'none';
+    document.getElementById('errorpwModal').style.display = 'none';
+
     if (type === 'success') {
       document.getElementById('successModal').style.display = 'block';
+    } else if (type === 'errorid') {
+      document.getElementById('erroridModal').style.display = 'block';
+    } else if (type === 'errorpw') {
+      document.getElementById('errorpwModal').style.display = 'block';
     } else {
       document.getElementById('errorModal').style.display = 'block';
     }
   }
-  function closeModal() {
-    document.getElementById('modalOverlay').style.display = 'none';
-    document.getElementById('successModal').style.display = 'none';
-    document.getElementById('errorModal').style.display = 'none';
-  }
+
+  
+function closeModal() {
+      document.getElementById('modalOverlay').style.display = 'none';
+      document.getElementById('successModal').style.display = 'none';
+	  document.getElementById('erroridModal').style.display = 'none';
+	  document.getElementById('errorpwModal').style.display = 'none';
+      document.getElementById('errorModal').style.display = 'none';
+    }
