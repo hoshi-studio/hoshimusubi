26b9d26ae6e0ab1bf1a931f15563e035e672a55f
SUHWA0904 <102892396+SUHWA0904@users.noreply.github.com>
2025-06-25 12:29:53 +0900

    css

--- src/main/java/com/hoshimusubi/seunga/test/PasswordEncoderExample.java
+++ src/main/java/com/hoshimusubi/seunga/test/PasswordEncoderExample.java
@@ -5,7 +5,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class PasswordEncoderExample {
     public static void main(String[] args) {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        String rawPassword = "1234";
+        String rawPassword = "pass1234!";
         //테스트용 석중 비밀번호 String rawPassword = "password1234";
         String encodedPassword = encoder.encode(rawPassword);
         System.out.println(encodedPassword);
